c8f695fa85fe63bcab5f5a3b51363b907caa4c28
PedroJoaoPereira <pedro.pereira.95@hotmail.com>
2016-05-13 09:50:25 +0100

    Protocolos done

--- V1.0/SDIS2CERT/src/Utilities/Misc.java
+++ V1.0/SDIS2CERT/src/Utilities/Misc.java
@@ -1,6 +1,6 @@
 package Utilities;
 
-import java.util.List;
+import java.io.File;
 
 public class Misc {
 
@@ -12,4 +12,48 @@ public class Misc {
 	    return builder.toString();
 	}
 	
+	static private void deleteInstance(File instance){
+		File[] files = instance.listFiles();
+
+		if(files != null){
+			for(File file : files){
+				if(file == null) continue;
+				
+				if(file.isDirectory())
+					deleteInstance(file);
+				else
+					file.delete();
+			}
+		}
+		
+		File parentFolder = new File(ProgramDefinitions.mydata.peerID + File.separator + instance.getParentFile().getName());
+		
+		instance.delete();
+		
+		if(parentFolder != null && folderSize(parentFolder) == 0) parentFolder.delete();
+	}
+
+	static private long folderSize(File folder){
+		if(folder == null || !folder.isDirectory()) return 0;
+		
+		long length = 0;
+		for(File file : folder.listFiles()){
+			if(file == null) continue;
+			
+			if(file.isFile())
+				length += file.length();
+			else
+				length += folderSize(file);
+		}
+		return length;
+	}
+	
+	static public boolean deleteFolder(String fileId){
+		String filesDir = ProgramDefinitions.mydata.peerID + File.separator + fileId;
+		File chunkFolder = new File(filesDir);
+		
+		folderSize(chunkFolder);
+		deleteInstance(chunkFolder);
+		return true;
+	}
 }
